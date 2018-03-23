2aea2447f2eb8d562d142dc0a8b379eeaa7797af
tff <745332367@qq.com>
2018-03-23 22:53:41 +0800

    添加注释文档

--- src/main/java/com/job2/entity/BaseEntity.java
+++ src/main/java/com/job2/entity/BaseEntity.java
@@ -4,7 +4,11 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
-
+/**
+ * 
+ * @author acer-pc
+ *
+ */
 @MappedSuperclass
 public class BaseEntity {
 	private Long id;
